5dd13f406ec2f9261d0a716d387d4dcdfbe73a1d
Uthea <gstsadhu@gmail.com>
2021-11-09 16:09:57 +0700

    refactor docs

--- 016_h8ocbc_converter/main.py
+++ 016_h8ocbc_converter/main.py
@@ -1,32 +1,54 @@
 def celcius2kelvin(celcius):
+    """
+    this function convert celcius to kelvin
+    :param celcius: int or float
+    :return float in kelvin
+    """
     return celcius + 273.15
 
 
 def kelvin2celcius(kelvin):
+    """
+    this function convert kelvin to celcius
+    :param kelvin: int or float
+    :return float in celcius
+    """
     return kelvin - 273.15
 
 
 def to_fahrenheit(value, temp_type):
-    assert temp_type in ['kelvin', 'celcius'], "type must be kelvin or celcius"  # make sure to limit the options
+    """
+    this function convert kelvin or celcius value into fahrenheit
+    :param value: int or float
+    :param temp_type: dictate whether value is 'kelvin' or 'celcius'
+    :return int or float in fahrenheit
+    """
+    assert temp_type in ['kelvin', 'celcius'], "type must be kelvin or celcius"
     temp_value = 0
-    if temp_type == "kelvin":  # if input value is kelvin then convert it first to celcius
+    if temp_type == "kelvin":
         temp_value = kelvin2celcius(value)
     else:
-        temp_value = value  # if already in celcius, just assign the value
+        temp_value = value
 
-    return (temp_value * 9 / 5) + 32  # celcius to fahrenheit equation
+    return (temp_value * 9 / 5) + 32  #
 
 
 def from_fahrenheit(value, output_temp_type):
-    assert output_temp_type in ['kelvin', 'celcius'],"type must be kelvin or celcius"  # make sure to limit the options
+    """
+    this function convert fahrenheit value into kelvin or celcius
+    :param value: int or float
+    :param output_temp_type: dictate return value is in 'kelvin' or 'celcius'
+    :return int or float in kelvin or celcius (depends on output_temp_type param)
+    """
+    assert output_temp_type in ['kelvin', 'celcius'],"type must be kelvin or celcius"
 
-    celcius_value = (value - 32) * (5 / 9)  # convert to celcius by default
+    celcius_value = (value - 32) * (5 / 9)
 
-    if output_temp_type == "kelvin":  # if output temperature type is kelvin then convert celcius to kelvin
-        return celcius2kelvin(celcius_value)  # convert to kelvin and return it
+    if output_temp_type == "kelvin":
+        return celcius2kelvin(celcius_value)
 
-    return celcius_value  # if output type is celcius then no further processing needed
+    return celcius_value
 
 
 if __name__ == '__main__':
-    print(from_fahrenheit(20, 'celcius'))
+    print(from_fahrenheit(20, "kelvin"))
